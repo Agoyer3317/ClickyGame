b39c63240fc03ed67b9b274b375bc6ecab16cb74
Allison Scollan <allisonscollan@Allisons-MacBook-Pro.local>
2018-10-07 21:53:17 -0400

    Updates

---
+++ precache-manifest.0f263d19aa317af057a9795cbb88e117.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "a24e10183b7c9e9bd862",
+    "url": "/ClickyGame/static/js/main.a24e1018.chunk.js"
+  },
+  {
+    "revision": "97b37ff4fca74c5d0ccf",
+    "url": "/ClickyGame/static/js/1.97b37ff4.chunk.js"
+  },
+  {
+    "revision": "5d80097d0c6b64516017cdc5f7b6b991",
+    "url": "/ClickyGame/index.html"
+  }
+];
